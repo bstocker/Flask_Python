b7799be85a17ee51cc0a5fbcbb64635375839509
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-02-26 10:15:21 +0100

    Restructuration de l'application

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-  app.run(debug = True)
 
 @app.route('/')
 def index():
@@ -13,3 +9,6 @@ def index():
 @app.route('/exercices_base1')
 def exercices_base1():
     return render_template('exercices_base1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
