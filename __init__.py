f9555e7f7453a41b16e0f4abc76f9459452b8cf9
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-02-26 10:06:39 +0100

    Réparation du démarrage en mode DEBUG.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,9 @@ from flask import render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+  app.run(debug = True)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -10,6 +13,3 @@ def index():
 @app.route('/exercices_base1')
 def exercices_base1():
     return render_template('exercices_base1.html')
-
-if __name__ == '__main__':
-  app.run(debug = True)
